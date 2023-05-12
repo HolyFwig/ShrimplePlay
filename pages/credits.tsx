cbdb3e3e52151a3d18f13c523004eccd627caad3
Amy <amyd09@proton.me>
2023-05-12 11:42:32 -0500

    Update credits.tsx

--- pages/credits.tsx
+++ pages/credits.tsx
@@ -13,7 +13,7 @@ const Home: NextPage = () => {
         <meta name="keywords" content="Lunadox, Amy, Celestial, Games, Roblox, School, Chromebook, Unblocked, Proxies, Portfolio, Developer, Frontend, HTML5, CSS3, JS" />
         <meta name="author" content="Copyright ⓒ 2022 Celestial" />
         <meta name="theme-color" content="#ffc0cb" />
-        <meta property="og:title" content="Celestial Gaming" />
+        <meta property="og:title" content="ShrimplePlay" />
         <meta property="og:locale" content="en-US" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://play.thecelestial.gq" />
@@ -24,7 +24,7 @@ const Home: NextPage = () => {
       <nav className={styles.navbar}>
         <div className={styles.logo}>
           <Link href="/">
-            <a>Celestial Gaming</a>
+            <a>ShrimplePlay</a>
           </Link>
         </div>
         <div className={styles.hotlinks}>
@@ -44,23 +44,19 @@ const Home: NextPage = () => {
       </nav>
       <main className={styles.main}>
         <u>Credits</u>
-        <h2>Lunadox ✿#0206 - Lead Developer</h2>
-        <h2>Hiro🔥#4504 - Hosting</h2>
+        <h2>ShrimpleFix - Lead Developer</h2>
         <h2>sexyduceduce#6722/Carcal.js - Creator of Ultraviolet</h2>
       </main>
       <div className={styles.socials}>
-        <a href="https://discord.gg/Kn63hp3WF4">
+        <a href="https://discord.gg/yv83w6QeF6">
           <img src="/imgs/discord.png" width="30" height="30" />
         </a>
-        <a href="https://github.com/Celestial-Dev/Celestial-Gaming">
+        <a href="https://github.com/Lunadox-Dev/Celestial-Gaming">
           <img src="/imgs/github.svg" width="30" height="30" />
-        </a>
-        <a href="https://thecelestial.org/">
-          <img src="/imgs/web.svg" width="30" height="30" />
         </a>
       </div>
       <footer className={styles.copyright}>
-        <p>Copyright © Celestial 2022</p>
+        <a href="https://shrimple.club">shrimple.club</a>
       </footer>
     </div>
   )
