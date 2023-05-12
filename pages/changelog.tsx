0dfea74137429e9f89cad84220d119c89cbfc04c
Amy <amyd09@proton.me>
2023-05-12 11:47:01 -0500

    Update changelog.tsx

--- pages/changelog.tsx
+++ pages/changelog.tsx
@@ -9,32 +9,28 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Celestial Gaming is a free to use, open-source, gaming site with a large collection of html5, flash, mobile, and retro games to play out of the box in your browser! WARNING: I will not take responsibility if this becomes a distraction in the classroom, nor will I take if this gets a student in trouble." />
-        <meta name="keywords" content="Lunadox, Amy, Celestial, Games, Roblox, School, Chromebook, Unblocked, Proxies, Portfolio, Developer, Frontend, HTML5, CSS3, JS" />
-        <meta name="author" content="Copyright ⓒ 2022 Celestial" />
+        <meta name="description" content="ShrimplePlay is a free to use, open-source, gaming site with a large collection of html5, flash, mobile, and retro games to play out of the box in your browser!" />
+        <meta name="keywords" content="ShrimpleFix, Amy, Celestial, Games, Roblox, School, Chromebook, Unblocked, Proxies, Portfolio, Developer, Frontend, HTML5, CSS3, JS" />
+        <meta name="author" content="Copyright ⓒ 2022-2023 ShrimpleFix" />
         <meta name="theme-color" content="#ffc0cb" />
-        <meta property="og:title" content="Celestial Gaming" />
+        <meta property="og:title" content="ShrimplePlay" />
         <meta property="og:locale" content="en-US" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://play.thecelestial.gq" />
-        <meta property="og:description" content="Celestial Gaming is a free to use, open-source, gaming site with a large collection of html5, flash, mobile, and retro games to play out of the box in your browser! WARNING: I will not take responsibility if this becomes a distraction in the classroom, nor will I take if this gets a student in trouble." />
-        <title>Celestial Gaming</title>
+        <meta property="og:url" content="https://g.shrimple.club" />
+        <meta property="og:description" content="ShrimplePlay is a free to use, open-source, gaming site with a large collection of html5, flash, mobile, and retro games to play out of the box in your browser!" />
+        <title>ShrimplePlay</title>
         <link rel="icon" href="/imgs/favicon.ico" />
       </Head>
       <nav className={styles.navbar}>
         <div className={styles.logo}>
           <Link href="/">
-            <a>Celestial Gaming</a>
+            <a>ShrimplePlay</a>
           </Link>
         </div>
         <div className={styles.hotlinks}>
           <img src="/imgs/lib.svg" width="25" height="25" />
           <Link href="/library/">
             <a>Library!</a>
-          </Link>
-          <img src="/imgs/credits.svg" width="25" height="25" />
-          <Link href="/credits/">
-            <a>Credits!</a>
           </Link>
           <img src="/imgs/changelog.svg" width="25" height="25" />
           <Link href="/changelog/">
@@ -43,26 +39,17 @@ const Home: NextPage = () => {
         </div>
       </nav>
       <main className={styles.main}>
-        <u>Intial Release (11/26/22)</u>
-        <h2>- Added Homepage</h2>
-        <h2>- Added Library</h2>
-        <h2>- Added Credits</h2>
-        <h2>- Added Changelog</h2>
-        <h2>- Basically the birth of the entire project</h2>
+        <u>V1.1.2 (5/12/23)</u>
+        <h2>- Removed Credits</h2>
+        <h2>- Project revival planned</h2>
       </main>
       <div className={styles.socials}>
-        <a href="https://discord.gg/Kn63hp3WF4">
-          <img src="/imgs/discord.png" width="30" height="30" />
-        </a>
-        <a href="https://github.com/Celestial-Dev/Celestial-Gaming">
+        <a href="https://github.com/ShrimpleFix/ShrimplePlay">
           <img src="/imgs/github.svg" width="30" height="30" />
-        </a>
-        <a href="https://thecelestial.org/">
-          <img src="/imgs/web.svg" width="30" height="30" />
         </a>
       </div>
       <footer className={styles.copyright}>
-        <p>Copyright © Celestial 2022</p>
+        <a href="https://shrimple.club">shrimple.club</a>
       </footer>
     </div>
   )
