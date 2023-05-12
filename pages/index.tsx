a3341963d84caee9b69bb9fc8e3e17eb17878da1
Amy <amyd09@proton.me>
2023-05-12 11:42:06 -0500

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,22 +9,22 @@ const Home: NextPage = () => {
     <div className={styles.container}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Celestial Gaming is a free to use, open-source, gaming site with a large collection of html5, flash, mobile, and retro games to play out of the box in your browser! WARNING: I will not take responsibility if this becomes a distraction in the classroom, nor will I take if this gets a student in trouble." />
+        <meta name="description" content="ShrimplePlay is a free to use, open-source, gaming site with a large collection of html5, flash, mobile, and retro games to play out of the box in your browser!" />
         <meta name="keywords" content="Lunadox, Amy, Celestial, Games, Roblox, School, Chromebook, Unblocked, Proxies, Portfolio, Developer, Frontend, HTML5, CSS3, JS" />
-        <meta name="author" content="Copyright ⓒ 2022 Celestial" />
+        <meta name="author" content="Copyright ⓒ 2022-2023 ShrimpleFix" />
         <meta name="theme-color" content="#ffc0cb" />
-        <meta property="og:title" content="Celestial Gaming" />
+        <meta property="og:title" content="ShrimplePlay" />
         <meta property="og:locale" content="en-US" />
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://play.thecelestial.gq" />
-        <meta property="og:description" content="Celestial Gaming is a free to use, open-source, gaming site with a large collection of html5, flash, mobile, and retro games to play out of the box in your browser! WARNING: I will not take responsibility if this becomes a distraction in the classroom, nor will I take if this gets a student in trouble." />
-        <title>Celestial Gaming</title>
+        <meta property="og:url" content="https://play.shrimple.club" />
+        <meta property="og:description" content="ShrimplePlay is a free to use, open-source, gaming site with a large collection of html5, flash, mobile, and retro games to play out of the box in your browser!" />
+        <title>ShrimplePlay</title>
         <link rel="icon" href="/imgs/favicon.ico" />
       </Head>
       <nav className={styles.navbar}>
         <div className={styles.logo}>
           <Link href="/">
-            <a>Celestial Gaming</a>
+            <a>ShrimplePlay</a>
           </Link>
         </div>
         <div className={styles.hotlinks}>
@@ -61,18 +61,14 @@ const Home: NextPage = () => {
         </div>
       </main>
       <div className={styles.socials}>
-        <a href="https://discord.gg/Kn63hp3WF4">
-          <img src="/imgs/discord.png" width="30" height="30" />
-        </a>
-        <a href="https://github.com/Celestial-Dev/Celestial-Gaming">
+<a href="https://github.com/Lunadox-Dev/Celestial-Gaming">
           <img src="/imgs/github.svg" width="30" height="30" />
-        </a>
-        <a href="https://thecelestial.org/">
-          <img src="/imgs/web.svg" width="30" height="30" />
         </a>
       </div>
       <footer className={styles.copyright}>
-        <p>Copyright © Celestial 2022</p>
+        <Link href="https://shrimple.club">
+          <a>shrimple.club</a>
+        </Link>
       </footer>
     </div>
   )
